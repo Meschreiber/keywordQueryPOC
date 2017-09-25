119899f509e76978e283142af2863c0aa1d0921f
Maria Schreiber <meschreiber3@gmail.com>
2017-09-25 13:45:13 +0200

    Additional basic widgets'

--- app.js
+++ app.js
@@ -4,21 +4,29 @@ var search = instantsearch({
     apiKey: 'fe2708f4939640ae043e0a04008fbb10', // search only API key, no ADMIN key
     indexName: 'instant_search',
     urlSync: true
-  });
+});
 
-  search.addWidget(
+search.addWidget(
     instantsearch.widgets.searchBox({
-      container: '#search-input'
+        container: '#search-input'
     })
-  );
+);
 
-  search.addWidget(
+search.addWidget(
     instantsearch.widgets.hits({
-      container: '#hits',
-      hitsPerPage: 10,
-      templates: {
-        item: document.getElementById('hit-template').innerHTML,
-        empty: "We didn't find any results for the search <em>\"{{query}}\"</em>"
-      }
+        container: '#hits',
+        hitsPerPage: 10,
+        templates: {
+            item: document.getElementById('hit-template').innerHTML,
+            empty: "We didn't find any results for the search <em>\"{{query}}\"</em>"
+        }
     })
-  );
+);
+
+search.addWidget(
+    instantsearch.widgets.pagination({
+        container: '#pagination'
+    })
+);
+
+search.start();
--- import.js
+++ import.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const algoliasearch = require('algoliasearch');
 
 const client = algoliasearch("5NICTDJ5Q3", "957914327c4dd35e7de940940b2757ad");
@@ -5,10 +7,13 @@ const index = client.initIndex('keyword_poc');
 
 index.setSettings({
     searchableAttributes: [
-      'brand',
-      'name',
-      'categories',
-      'unordered(description)',
+        'brand',
+        'name',
+        'categories',
+        'unordered(description)',
     ],
     customRanking: ['desc(popularity)'],
-  });
+});
+
+const jsonData = JSON.parse(fs.readFileSync(path.join(__dirname, 'bestbuy_light.json'), 'utf8'));
+index.addObjects(jsonData, err => { if(err) console.error(err)});
